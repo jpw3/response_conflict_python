e3fcc4469815b356563d5d55b6bc2687e568b1ca
James <jpwilmott3@yahoo.com>
2018-02-06 16:55:18 -0500

    computing the data for the congruency conditions aggregated across trial types within the respective condition. About to pull them out for each individual trial type

--- rc_lr_analysis.py
+++ rc_lr_analysis.py
@@ -192,7 +192,36 @@ def computeNT(trial_matrix, id='agg'):
 				db['%s_LR_%s_%s_targets_pc_bs_sems'%(id,type,nrt)] = compute_BS_SEM(res_matrix, 'pc');			
 	db.sync();			
 			
-
+def computeCongruency(trial_matrix, id = 'agg'):
+	#computes the response and perceptual incongruence data for the two target trials
+	#trial_matrix should be a list of trials for each subjects
+	#get appropriate database to store data
+	if id=='agg':
+		db=subject_data;
+	else:
+		db=individ_subject_data;	
+    #here cycle through the total number of stimuli and number of distractors, finding the RT and accuracy for each combo
+	#run this analysis separatel for the bottom up and top down blocks
+	for type in ['b','t']: 
+		#cycle through the different types: resp cong, perc cong; resp cong, perc incong; respon incong, percept incong
+		for trial_types, name in zip([(17,18,19,20),(21,22),(23,24,25,26)],['cong_per_cong_resp','incong_per_cong_resp','incong_per_incong_resp']):
+			t_matrix = [[tee for tee in trs if (tee.block_type==type)] for trs in trial_matrix];
+			all_rt_matrix = [[tee.response_time for tee in ts if((tee.result==1)&(tee.nr_targets==2)&(tee.trial_type in trial_types))] for ts in t_matrix];
+			ind_rt_sds=[std(are) for are in all_rt_matrix];  #get individual rt sds and il sds to 'shave' the rts of extreme outliers
+			rt_matrix=[[r for r in individ_rts if (r>=(mean(individ_rts)-(3*ind_rt_sd)))&(r<=(mean(individ_rts)+(3*ind_rt_sd)))] for individ_rts,ind_rt_sd in zip(all_rt_matrix,ind_rt_sds)]; #trim matrixed rts of outliers greater than 3 s.d.s from the mean			
+			res_matrix = [[tee.result for tee in ts if((tee.nr_targets==2)&(tee.trial_type in trial_types))] for ts in t_matrix];
+			rts = [r for y in rt_matrix for r in y]; res = [s for y in res_matrix for s in y];
+			if len(rts)==0:
+				continue; #skip computing and saving data if there was no data that matched the criteria (so the array is empty)			
+			db['%s_LR_%s_2_targets_%s_mean_rt'%(id,type,name)]=mean(rts);	db['%s_LR_%s_2_targets_%s_median_rt'%(id,type,name)]=median(rts);	db['%s_LR_%s_2_targets_%s_var_rt'%(id,type,name)]=var(rts);
+			db['%s_LR_%s_2_targets_%s_pc'%(id,type,name)]=pc(res);
+			print '%s %s %s = rt: %3.2f'%(id,type,name,mean(rts));			
+			db.sync();
+			if id=='agg':
+				db['%s_LR_%s_2_targets_%s_rt_bs_sems'%(id,type,name)] = compute_BS_SEM(rt_matrix,'time');
+				db['%s_LR_%s_2_targets_%s_pc_bs_sems'%(id,type,name)] = compute_BS_SEM(res_matrix, 'pc');			
+	db.sync();				
+				
 
 def compute_BS_SEM(data_matrix, type):
     #calculate the between-subjects standard error of the mean. data_matrix should be matrix of trials including each subject
